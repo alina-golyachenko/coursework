dd1abbfda6f9a252ed1a0155f23d554a687b76c7
Alina Golyachenko <ipz202_gaa@student.ztu.edu.ua>
2022-04-07 11:45:08 +0200

    changed config

--- Config/config.php
+++ Config/config.php
@@ -7,10 +7,10 @@ $Config =
     [
         'Database' =>
         [
-            'Server' => 'localhost',
-            'Username' => 'root',
-            'Password' => '',
-            'Database' => 'coursework'
+            'Server' => 'eu-cdbr-west-02.cleardb.net',
+            'Username' => 'b176f69cb2a973',
+            'Password' => 'a8d63bcf',
+            'Database' => 'heroku_d5b543b213dc8d5'
         ],
         'RecipesCount' => 10
     ]
